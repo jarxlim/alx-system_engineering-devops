9a8f2e5347bbb3cf2ef3fa3e510ed50301da24c7
root <chimachike1@gmail.com>
2023-10-23 15:05:46 -0700

    1-export_to_CSV.py

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""REST API for todo lists of employees"""
+import csv
+import requests
+from sys import argv
+
+if __name__ == '__main__':
+    user_id = argv[1]
+    user = requests.get("https://jsonplaceholder.typicode.com/users/{}".
+                        format(user_id), verify=False).json()
+    todo = requests.get("https://jsonplaceholder.typicode.com/todos?userId={}".
+                        format(user_id), verify=False).json()
+
+    with open("{}.csv".format(user_id), 'w', newline='') as csvfile:
+        csvwriter = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+        for task in todo:
+            csvwriter.writerow([int(user_id), user.get('username'),
+                                task.get('completed'),
+                                task.get('title')])
