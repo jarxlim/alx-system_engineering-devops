9a8e0d166cd2b864d4a83d04751692adba49fd23
root <chimachike1@gmail.com>
2023-10-23 14:34:59 -0700

    0-gather_data_from_an_API.py

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""REST API for todo lists of employees"""
+
+import requests
+import sys
+
+
+if __name__ == '__main__':
+    employeeId = sys.argv[1]
+    employee = requests.get("https://jsonplaceholder.typicode.com/users/{}".
+                        format(employeeId), verify=False).json()
+    todo = requests.get("https://jsonplaceholder.typicode.com/todos?userId={}".
+                        format(employeeId), verify=False).json()
+
+    done_tasks = []
+    for task in todo:
+        if task.get('completed') is True:
+            done_tasks.append(task.get('title'))
+    print("Employee {} is done with tasks({}/{}):"
+          .format(user.get('name'), len(done_tasks), len(todo)))
+
+    for task in done_tasks:
+        print("\t {}".format(task))
