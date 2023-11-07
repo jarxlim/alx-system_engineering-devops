38c15157620407856d196d4b9b0d2475a42290ef
root <chimachike1@gmail.com>
2023-11-07 12:15:35 -0800

    done

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""this module is a recursive function that queries the Reddit API,
+"""
+this module is a recursive function that queries the Reddit API,
 parses the title of all hot articles, and prints sorted count of given keyword"""
 
 import json
--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
 this module contains a recursive function that queries the Reddit API and
-returns a list containing the titles of all hot articles for a given subreddit.
+returns a list containing the titles of all hot articles for a given subreddit
 """
 
 import requests
